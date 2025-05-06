672a6c9b59eb2e5bb36dbd05ad7576be5995d15d
Yijun Lin - Win <linyijun@yijunstudio.xyz>
2025-05-06 20:18:20 +0800

    更新灯光控制逻辑，修改红灯控制命令以同时关闭红灯和蜂鸣器，并优化报警状态切换的注释信息。

--- src/controllers/lightController.ts
+++ src/controllers/lightController.ts
@@ -7,8 +7,11 @@ import { logger } from "../utils/logger";
  */
 export async function setNormalState(req: Request, res: Response) {
   try {
-    // 关闭红灯: A0 03 00 A3
-    await sendHexCommand([0xa0, 0x03, 0x00, 0xa3]);
+    // 关闭红灯+蜂鸣: A0 07 00 A7
+    await sendHexCommand([0xa0, 0x07, 0x00, 0xa7]);
+
+    // // 关闭红灯: A0 03 00 A3
+    // await sendHexCommand([0xa0, 0x03, 0x00, 0xa3]);
 
     // 打开绿灯: A0 02 01 A3
     await sendHexCommand([0xa0, 0x02, 0x01, 0xa3]);
@@ -37,6 +40,9 @@ export async function setAlarmState(req: Request, res: Response) {
   try {
     // 关闭绿灯: A0 02 00 A2
     await sendHexCommand([0xa0, 0x02, 0x00, 0xa2]);
+
+    // // 打开红灯: A0 03 01 A4
+    // await sendHexCommand([0xa0, 0x03, 0x01, 0xa4]);
 
     // 打开红灯+蜂鸣: A0 07 01 A8
     await sendHexCommand([0xa0, 0x07, 0x01, 0xa8]);
